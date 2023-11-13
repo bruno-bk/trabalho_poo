146a9e149ab37a8cc90024ff6a5bd9067427e2d1
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-11-12 22:16:21 -0300

    Montando o ambiente com spring boot

---
+++ src/main/java/com/example/Trabalho_POO/TrabalhoPooApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trabalho_POO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoPooApplication.class, args);
+	}
+
+}
