13c8a52bfa159d45bbcd4c21819ca56cc786e967
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-11-15 00:51:47 -0300

    Adicionando o método POST

--- src/main/java/com/todolist/ToDoList/controler/TaskControler.java
+++ src/main/java/com/todolist/ToDoList/controler/TaskControler.java
@@ -3,8 +3,12 @@ package com.todolist.ToDoList.controler;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.todolist.ToDoList.model.Task;
@@ -21,4 +25,10 @@ public class TaskControler {
 	public List<Task> getTasks() {
 		return taskRepository.findAll();
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
 }
