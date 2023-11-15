245a5fd0b89d2dff3e7edf7a219961ae2bbb5453
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-11-14 22:20:27 -0300

    Adicionando os métodos PUT e DELETE

--- src/main/java/com/example/Trabalho_POO/controler/clientControler.java
+++ src/main/java/com/example/Trabalho_POO/controler/clientControler.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -31,4 +34,19 @@ public class ClientControler {
 	public Cliente adicionar(@RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
+	
+	@PutMapping("/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+		Cliente clinteSalvo = clienteRepository.findById(id).orElse(null);
+		if (clinteSalvo != null) {
+        	clinteSalvo.setNome(cliente.getNome());
+            return clienteRepository.save(clinteSalvo);
+        }
+        return null;
+    }
+	
+	@DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+		clienteRepository.deleteById(id);
+    }
 }
