ae67e82247f37e5ca321fac17669bed882811081
Bruno Bilhar Karaziack <brunobk1260@outlook.com>
2023-11-15 12:18:58 -0300

    Adicionando os métodos PUT e DELETE

--- src/main/java/com/todolist/ToDoList/controler/TaskControler.java
+++ src/main/java/com/todolist/ToDoList/controler/TaskControler.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,5 +34,27 @@ public class TaskControler {
 	@ResponseStatus(HttpStatus.CREATED)
     public Task createTask(@RequestBody Task task) {
         return taskRepository.save(task);
+    }
+	
+	@PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
+        Task existingTask = taskRepository.findById(id).orElse(null);
+        if (existingTask != null) {
+            existingTask.setDescription(updatedTask.getDescription());
+            existingTask.setCompleted(updatedTask.isCompleted());
+            Task updated = taskRepository.save(existingTask);
+            return ResponseEntity.ok(updated);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Task> deleteTask(@PathVariable Long id) {
+    	Task existingTask = taskRepository.findById(id).orElse(null);
+    	if (existingTask != null) {
+    		taskRepository.deleteById(id);
+    		return ResponseEntity.ok(existingTask);
+    	}
+    	return ResponseEntity.notFound().build();
     }
 }
